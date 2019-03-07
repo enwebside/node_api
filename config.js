c7a2b31ed9d8ccfeda98a76a9cd5845b275c7c51
David Aasen <46817870+enwebside@users.noreply.github.com>
2019-03-07 11:48:33 +0100

    remove info personal

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 3000,
     URL : process.env.URL || 'http://localhost:3000',
-    MONGODB_URI : process.env.MONGODB_URI || 'mongodb+srv://bruker:7ZJzBfvmieS4fsh@nodeapi-plyoo.mongodb.net/test?retryWrites=true',
-    JWT_SECRET: process.env.JWT_SECRET || 'engodhest'
+    MONGODB_URI : process.env.MONGODB_URI || '',
+    JWT_SECRET: process.env.JWT_SECRET || ''
 
 
 }
