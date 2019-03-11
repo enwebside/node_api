083fc569a6aeed1ca2af9d5cb0363ca781bd84dc
David Aasen <46817870+enwebside@users.noreply.github.com>
2019-03-11 12:08:45 +0100

    edit cors

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 server.use(cors());
 
+const corsOptions = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "allowedHeaders":"Content-Type,Authorization",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}
+
 server.listen(config.PORT, () => {
     mongoose.set('useFindAndModify', false);
     mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true});
