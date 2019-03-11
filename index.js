731477de629c6b42630bef1281628890945f7f87
David Aasen <46817870+enwebside@users.noreply.github.com>
2019-03-11 12:44:16 +0100

    chance

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ const cors = require('cors');
 const server = restify.createServer();
 
 server.use(restify.plugins.bodyParser());
-server.use(cors());
+
 
 const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders:'Content-Type,Authorization',
+  origin: ["*"],
+  methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
+  allowedHeaders:["Content-Type","Authorization"],
   preflightContinue: false,
   optionsSuccessStatus: 204
 }
+server.use(cors());
 
 server.listen(config.PORT, () => {
     mongoose.set('useFindAndModify', false);
